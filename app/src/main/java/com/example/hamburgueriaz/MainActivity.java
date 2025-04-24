d535266422287abb3bcf07c47af36b07bb099887
httpmurilo <muriloeduardo1997@outlook.com>
2025-04-23 21:35:46 -0300

    continuacao

--- app/src/main/java/com/example/hamburgueriaz/MainActivity.java
+++ app/src/main/java/com/example/hamburgueriaz/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.hamburgueriaz;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int quantity = 0;
+    private final int BASE_PRICE = 10;
+    private EditText editTextName;
+    private CheckBox checkBacon, checkQueijo, checkOnion;
+    private TextView textQuantity, textTotal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editTextName = findViewById(R.id.editTextName);
+        checkBacon = findViewById(R.id.checkBacon);
+        checkQueijo = findViewById(R.id.checkQueijo);
+        checkOnion = findViewById(R.id.checkOnion);
+        textQuantity = findViewById(R.id.textQuantity);
+        textTotal = findViewById(R.id.textTotal);
+
+        Button buttonMinus = findViewById(R.id.buttonMinus);
+        Button buttonPlus = findViewById(R.id.buttonPlus);
+        Button buttonOrder = findViewById(R.id.buttonOrder);
+
+        buttonMinus.setOnClickListener(v -> {
+            if (quantity > 0) quantity--;
+            updateUI();
         });
+
+        buttonPlus.setOnClickListener(v -> {
+            quantity++;
+            updateUI();
+        });
+
+        buttonOrder.setOnClickListener(v -> {
+            String name = editTextName.getText().toString();
+            String orderSummary = "Cliente: " + name + "\nTotal: R$" + calculatePrice();
+            Toast.makeText(this, orderSummary, Toast.LENGTH_LONG).show();
+        });
+    }
+
+    private void updateUI() {
+        textQuantity.setText(String.valueOf(quantity));
+        textTotal.setText("Resumo do pedido: R$" + calculatePrice());
+    }
+
+    private int calculatePrice() {
+        int price = BASE_PRICE;
+        if (checkBacon.isChecked()) price += 2;
+        if (checkQueijo.isChecked()) price += 1;
+        if (checkOnion.isChecked()) price += 3;
+        return price * quantity;
     }
 }
